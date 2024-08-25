66e98fa3492a62c0430a06089b72b1daa867c96c
Mahith <mahith447@gmail.com>
2024-08-25 12:46:28 +0530

    Initial commit

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,11 @@ const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 require('dotenv').config();  // Load environment variables from .env file
 const helmet = require('helmet');
+
+// Initialize Express app
+const app = express();
 app.use(helmet());
 
-const app = express();
 const port = process.env.PORT || 3000;  // Use environment variable for port, default to 3000
 
 // MongoDB connection using environment variable
@@ -33,13 +35,13 @@ app.use(express.static(path.join(__dirname, '../frontend')));  // Serve static f
 // Routes
 app.use('/api', apiRoutes);
 
-// Start server
-app.listen(port, () => {
-  //console.log(`Server running at http://localhost:${port}`);
-});
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack); // Log the error details
-    res.status(500).send('Something went wrong!'); // Send a user-friendly error message
-  });
-  
+  console.error(err.stack); // Log the error details
+  res.status(500).send('Something went wrong!'); // Send a user-friendly error message
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
